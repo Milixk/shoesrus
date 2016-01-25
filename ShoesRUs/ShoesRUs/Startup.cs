d89555612ea50e134e95ade8606765f2f65338e0
Milixk <morgan.lovett@hotmail.co.uk>
2016-01-25 19:53:57 +0100

    Added comments to Startup class.

--- ShoesRUs/ShoesRUs/Startup.cs
+++ ShoesRUs/ShoesRUs/Startup.cs
@@ -11,10 +11,13 @@ namespace ShoesRUs
 {
     class Startup
     {
+        //Run the startup functions.
         public Startup()
         {
             DatabaseExist();
         }
+
+        //Check if the database exists locally, if not, download the database to the same directory as the application.
         private void DatabaseExist()
         {
             if (File.Exists(Application.StartupPath + @"\ShoesRUsDB.accdb") == false)
