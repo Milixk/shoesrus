e0f1b53f59f935f4f5e1fbea135cc11546b333da
Milixk <morgan.lovett@hotmail.co.uk>
2016-01-25 14:49:14 +0100

    First commit, has system files and database.

---
+++ ShoesRUs/ShoesRUs/DatabaseConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShoesRUs
+{
+    class DatabaseConnection
+    {
+        public static readonly string
+          dbsource = "Provider=Microsoft.ACE.OLEDB.12.0;" +
+          @"Data Source = " + Application.StartupPath +
+          @"\ShoesRUsDB.accdb";
+    }
+}
---
+++ ShoesRUs/ShoesRUs/MainForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace ShoesRUs
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
